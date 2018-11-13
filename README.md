736001d14d1d12d6d1056e891352d71f95fd90a0
Virko12345 <virkolomeo@gmail.com>
2018-11-13 23:17:40 +0200

    adedd lightbox

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# lightbox
+# Link to view "Lightbox" on the Github pages
+* [Lightbox](https://boxing199.github.io/lightbox/index.html)
---
+++ index.js
@@ -0,0 +1,8 @@
+$('[data-fancybox="gallery"]').fancybox({
+    protect: true, //protect images from downloading by right-click
+    slideShow: {
+        autoStart: true,
+        speed: 2000
+      },
+    
+});
